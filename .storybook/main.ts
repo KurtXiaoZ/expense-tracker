fcbe11858e6792fcd1ec638c2cd503078269e52b
KurtXiaoZ <kurtxiaoz@gmail.com>
2026-02-15 18:20:27 -0500

    chore: add storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,18 +1,14 @@
 import type { StorybookConfig } from '@storybook/nextjs-vite';
 
 const config: StorybookConfig = {
-  "stories": [
-    "../**/*.stories.tsx",
+  stories: ['../**/*.stories.tsx'],
+  addons: [
+    '@chromatic-com/storybook',
+    '@storybook/addon-vitest',
+    '@storybook/addon-a11y',
+    '@storybook/addon-docs',
   ],
-  "addons": [
-    "@chromatic-com/storybook",
-    "@storybook/addon-vitest",
-    "@storybook/addon-a11y",
-    "@storybook/addon-docs"
-  ],
-  "framework": "@storybook/nextjs-vite",
-  "staticDirs": [
-    "../public"
-  ]
+  framework: '@storybook/nextjs-vite',
+  staticDirs: ['../public'],
 };
 export default config;
